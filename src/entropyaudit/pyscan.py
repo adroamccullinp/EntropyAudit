6a1788aa85c8cb4c5dddea4a8112bffbd04a39ce
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2010-12-21 16:00:00 +0700

    chore: findings report

--- src/entropyaudit/pyscan.py
+++ src/entropyaudit/pyscan.py
@@ -210,3 +210,18 @@ class _Visitor(ast.NodeVisitor):
             canonical = self.imports.module_aliases.get(head, head)
             return canonical.split(".")[0] == "random"
         origin = self.imports.name_aliases.get(dotted)
+        return bool(origin and origin.split(".")[0] == "random")
+
+    def _hashlib_call_name(self, func: ast.AST) -> str | None:
+        """Return the hash algorithm name if func is a hashlib constructor.
+
+        Handles hashlib.md5(...), hashlib.new("md5"), and from hashlib import md5.
+        For hashlib.new the name comes from the first argument and is resolved by
+        the caller.
+        """
+        dotted = _dotted_name(func)
+        if dotted is None:
+            return None
+        head, _, leaf = dotted.rpartition(".")
+        if head:
+            canonical = self.imports.module_aliases.get(head, head)
