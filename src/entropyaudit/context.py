3286887c658d678c81b548fd4357da892e7e5ff4
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2011-02-22 17:53:00 +0700

    feat: add nonce hygiene

--- src/entropyaudit/context.py
+++ src/entropyaudit/context.py
@@ -62,3 +62,12 @@ def normalize(identifier: str) -> str:
     return identifier.replace("_", "").lower()
 
 
+def identifier_is_security_relevant(identifier: str) -> bool:
+    """True when identifier contains a security term.
+
+    "iv" and "key" are short and could appear inside unrelated words, so they
+    are matched as whole tokens split on underscores or camelCase boundaries,
+    while longer terms match as substrings.
+    """
+    if not identifier:
+        return False
