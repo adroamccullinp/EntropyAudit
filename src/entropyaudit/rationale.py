2f7af9ac26d4ca8bde4e33679090b9f114c17cbe
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2011-09-14 13:32:00 +0700

    chore: cwe-330

--- src/entropyaudit/rationale.py
+++ src/entropyaudit/rationale.py
@@ -39,3 +39,8 @@ RATIONALE = {
         "keystream and recover plaintext. Nonces must be unique per message and "
         "IVs must be random."
     ),
+    "EA005": (
+        "A fixed salt means identical inputs hash to identical digests across "
+        "all users and installs. This defeats the purpose of salting: an "
+        "attacker can precompute one rainbow table and reuse it everywhere, and "
+        "identical passwords become visibly identical. Generate a fresh random "
