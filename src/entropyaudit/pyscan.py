81b9ba5a6e9578ca43cd7fccf9ef1521d4763260
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2017-03-28 17:34:00 +0700

    style: clean up hardcoded keys

--- src/entropyaudit/pyscan.py
+++ src/entropyaudit/pyscan.py
@@ -166,3 +166,18 @@ class _Visitor(ast.NodeVisitor):
         Handles: import random; random.random(), the aliased form, and
         from random import randint style names. Returns the leaf callable name
         (for example "randint") or None.
+        """
+        dotted = _dotted_name(func)
+        if dotted is None:
+            return None
+        head, _, leaf = dotted.rpartition(".")
+        if head:
+            canonical = self.imports.module_aliases.get(head, head)
+            if canonical.split(".")[0] == "random" and leaf in RANDOM_CALLABLES:
+                return leaf
+            # random.Random().method style is covered by the constructor check.
+        else:
+            # bare name imported via "from random import randint"
+            origin = self.imports.name_aliases.get(dotted)
+            if origin and origin.split(".")[0] == "random":
+                leaf_name = origin.split(".")[-1]
