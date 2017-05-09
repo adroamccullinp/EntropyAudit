d94941013d1d3e6ef5900b480ffac3feb8557e7b
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2017-05-09 16:12:00 +0700

    chore: code scan

--- src/entropyaudit/context.py
+++ src/entropyaudit/context.py
@@ -71,3 +71,12 @@ def identifier_is_security_relevant(identifier: str) -> bool:
     """
     if not identifier:
         return False
+    normalized = normalize(identifier)
+    for term in SECURITY_TERMS:
+        if len(term) <= 3:
+            # Whole-token match for short terms to avoid false hits like
+            # "give" matching "iv" or "monkey" matching "key".
+            tokens = _split_tokens(identifier)
+            if term in tokens:
+                return True
+        elif term in normalized:
