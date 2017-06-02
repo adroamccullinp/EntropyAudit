c254773bb422c35d51dc2e13e4f0c7669a3c7218
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2017-06-02 16:07:00 +0700

    feat: review support

--- src/entropyaudit/rationale.py
+++ src/entropyaudit/rationale.py
@@ -34,3 +34,8 @@ RATIONALE = {
     ),
     "EA004": (
         "A nonce or initialization vector bound to a constant repeats on every "
+        "encryption. For stream ciphers and counter modes, reusing a nonce with "
+        "the same key lets an attacker xor two ciphertexts to cancel the "
+        "keystream and recover plaintext. Nonces must be unique per message and "
+        "IVs must be random."
+    ),
